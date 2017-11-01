0a44e514a482a7a6272c942dedf8c9535e340d17
Sam Upra <samupra@Sams-MacBook-Pro.local>
2017-11-01 23:04:30 +0700

    case class checker

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "caseclass-macro"
+
+version := "1.0"
+
+scalaVersion in ThisBuild := "2.11.8"
+
+organization := "com.net.samupra"
+
+libraryDependencies += "org.scala-lang" % "scala-reflect" % scalaVersion.value
---
+++ src/main/scala/com/net/samupra/macros/caseclasschecker/CaseClassChecker.scala
@@ -0,0 +1,20 @@
+package com.net.samupra.macros.caseclasschecker
+
+import scala.language.experimental.macros
+import scala.reflect.macros.blackbox
+
+object CaseClassChecker {
+
+  def impl[T : c.WeakTypeTag](c : blackbox.Context) = {
+    import c.universe._
+    val symbol = c.symbolOf[T]
+    (symbol.asClass.isCaseClass : @unchecked) match {
+      case false => c.abort(c.enclosingPosition, s"${symbol.fullName} must be a case class")
+      case true => c.Expr(Literal(Constant(true)))
+    }
+  }
+
+  def check[T] : Boolean = macro impl[T]
+
+}
+
