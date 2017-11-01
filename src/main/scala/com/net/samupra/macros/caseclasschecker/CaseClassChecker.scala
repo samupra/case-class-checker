a00e43bc458b77108d31473691a33a9788d63819
Sam Upra <samupra@Sams-MacBook-Pro.local>
2017-11-01 23:06:43 +0700

    remove unchecked

--- src/main/scala/com/net/samupra/macros/caseclasschecker/CaseClassChecker.scala
+++ src/main/scala/com/net/samupra/macros/caseclasschecker/CaseClassChecker.scala
@@ -8,7 +8,7 @@ object CaseClassChecker {
   def impl[T : c.WeakTypeTag](c : blackbox.Context) = {
     import c.universe._
     val symbol = c.symbolOf[T]
-    (symbol.asClass.isCaseClass : @unchecked) match {
+    symbol.asClass.isCaseClass match {
       case false => c.abort(c.enclosingPosition, s"${symbol.fullName} must be a case class")
       case true => c.Expr(Literal(Constant(true)))
     }
